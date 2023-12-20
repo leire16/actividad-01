db58f77dbd09398fa75671a7fc632300e55a5696
leire16 <leire.olabarri@gmail.com>
2023-12-20 16:41:26 +0100

    Añadir la clase C

---
+++ ClaseC.java
@@ -0,0 +1,28 @@
+// ClaseC.java
+
+public class ClaseC {
+    private double valor;
+    private boolean activo;
+
+    public ClaseC(double valor, boolean activo) {
+        this.valor = valor;
+        this.activo = activo;
+    }
+
+    // Métodos getter y setter (puedes generarlos automáticamente en muchos IDEs)
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+
+    public boolean isActivo() {
+        return activo;
+    }
+
+    public void setActivo(boolean activo) {
+        this.activo = activo;
+    }
+}
