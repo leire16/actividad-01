f3673390929e026b84977b7b9ef0da7e484c8346
leire16 <leire.olabarri@gmail.com>
2023-12-20 16:36:43 +0100

    Añadir la clase B

---
+++ ClaseB.java
@@ -0,0 +1,28 @@
+// ClaseB.java
+
+public class ClaseB {
+    private int numero;
+    private String descripcion;
+
+    public ClaseB(int numero, String descripcion) {
+        this.numero = numero;
+        this.descripcion = descripcion;
+    }
+
+    // Métodos getter y setter (puedes generarlos automáticamente en muchos IDEs)
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+}
